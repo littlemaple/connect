f0fe7a6540296d25d30e390f6554c14361f31607
lintao <lintao@medzone-biotech.com>
2016-03-10 09:53:13 +0800

    cm init

---
+++ app/src/main/java/connect/app/com/connect/Config.java
@@ -0,0 +1,10 @@
+package connect.app.com.connect;
+
+/**
+ * Created by 44260 on 2016/3/9.
+ */
+public class Config {
+//    public static boolean isDevelopMode = BuildConfig.DEVOLOP_MODE;
+//    public static boolean isTestMode = BuildConfig.TEST_MODE;
+//    public static String name = BuildConfig.name;
+}
