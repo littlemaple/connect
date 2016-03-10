e1b051b99ca346d215d33e1c42625a70c5b45c44
lintao <lintao@medzone-biotech.com>
2016-03-10 14:52:58 +0800

    cm pic

--- lib/build/generated/source/buildConfig/meizu/release/connect/app/com/lib/BuildConfig.java
+++ lib/build/generated/source/buildConfig/meizu/release/connect/app/com/lib/BuildConfig.java
@@ -8,11 +8,10 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "connect.app.com.lib";
   public static final String BUILD_TYPE = "release";
   public static final String FLAVOR = "meizu";
-  public static final int VERSION_CODE = 1;
-  public static final String VERSION_NAME = "1.0";
+  public static final int VERSION_CODE = -1;
+  public static final String VERSION_NAME = "";
   // Fields from product flavor: meizu
   public static final boolean DEVOLOP_MODE = true;
   public static final boolean TEST_MODE = true;
   public static final String name = "meizu";
-  // Fields from default config.
 }
--- lib/build/generated/source/buildConfig/xiaomi/release/connect/app/com/lib/BuildConfig.java
+++ lib/build/generated/source/buildConfig/xiaomi/release/connect/app/com/lib/BuildConfig.java
@@ -8,11 +8,10 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "connect.app.com.lib";
   public static final String BUILD_TYPE = "release";
   public static final String FLAVOR = "xiaomi";
-  public static final int VERSION_CODE = 1;
-  public static final String VERSION_NAME = "1.0";
+  public static final int VERSION_CODE = -1;
+  public static final String VERSION_NAME = "";
   // Fields from product flavor: xiaomi
   public static final boolean DEVOLOP_MODE = false;
   public static final boolean TEST_MODE = false;
   public static final String name = "xiaomi";
-  // Fields from default config.
 }
